c78b9109b518bc8708faa296da4e5d80e6bf5c80
Johan Ronsse <johan.ronsse@gmail.com>
2021-12-02 14:38:40 +0100

    Remove shadows, make more rounded. Change default color.

--- src/Component.svelte
+++ src/Component.svelte
@@ -6,7 +6,7 @@
 
   export let fontSize = '1rem'
   export let align = 'left'
-  export let checkedColor = '#bbe572'
+  export let checkedColor = '#32B0A2'
   export let borderTop = 'none'
   export let borderBottom = 'none'
   export let padding = '0'
@@ -69,7 +69,7 @@
     height: 1.875em;
     background-color: #eee;
     border: 0.0625em solid rgba(0, 0, 0, 0.15);
-    border-radius: 5px;
+    border-radius: 20px;
     transition: 0.35s cubic-bezier(0.785, 0.135, 0.15, 0.86);
     transition-property: background-color, border-color;
   }
@@ -90,15 +90,10 @@
     left: 0;
     margin: 2px;
     background-color: white;
-    border-radius: 5px;
-    box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.2), inset 1px 1px 3px rgba(255, 255, 255, 0.8);
+    border-radius: 100%;
     transition: 0.35s cubic-bezier(0.785, 0.135, 0.15, 0.86);
     transition-property: left, right;
     transition-delay: 0s, 0.05s;
-  }
-
-  .toggle-label:active .toggle-switch {
-    box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.2), inset 1px 1px 3px rgba(255, 255, 255, 0.8), inset 1px 1px 6px rgba(0, 0, 0, 0.1);
   }
 
   .toggle-label .toggle-input:checked ~ .toggle-track > .toggle-switch {
